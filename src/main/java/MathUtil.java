2fe99bc50bc6f88924c5454ac48daf1f992a036b
Lucas Garcia <lucasgarcia8595@hotmail.com>
2022-09-14 21:58:14 -0300

    testes até p7

--- src/main/java/MathUtil.java
+++ src/main/java/MathUtil.java
@@ -1,7 +1,27 @@
 public class MathUtil {
-    static int mdc(int a, int b){
+    public static int mdc(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+
+        //P6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+
+        //P1
         if (b > 0 && a % b == 0) {
             return b;
+        }
+
+        //P3
+        if (b == 0){
+            return Math.abs(a);
+        }
+
+        //P5
+        if (a % b != 0){
+            return 1;
         }
 
         return -1;
--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -9,8 +9,8 @@ public class MathUtilTest {
     @Test
     @Description("Se b > 0 é um divisor de a, então mdc(a,b)=b.")
     public void testMdcAParP1(){
-        final int a = 6;
-        final int b = 3;
+        final int a = 4;
+        final int b = 2;
         final int obtido = MathUtil.mdc(a, b);
         assertEquals(b, obtido);
     }
@@ -23,4 +23,117 @@ public class MathUtilTest {
         assertEquals(b, obtido);
     }
 
+    @Test
+    @Description("Se o mdc de A e B é divisível pelo divisor")
+    public void testMdcParesP2(){
+        final int a = 16;
+        final int b = 8;
+        final int divisor = 4;
+        final int obtido = MathUtil.mdc(a, b);
+        assertTrue(obtido % divisor == 0);
+    }
+
+    @Test
+    @Description("Se B = 0, então mdc(a,b) = a")
+    public void testMdcAPositivoP3(){
+        final int a = 15;
+        final int b = 0;
+        final int esperado = 15;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+
+    }
+
+    @Test
+    @Description("Se B = 0, então mdc(a,b) = a (positivo)")
+    public void testMdcANegativoP3(){
+        final int a = -30;
+        final int b = 0;
+        final int esperado = 30;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Se m = 0 || m > 0, então mdc(m.a, m.b) = m.mdc(a,b)")
+    public void testMdcMParP4(){
+        final int a = 9;
+        final int b = 3;
+        final int m = 2;
+        final int esperado = m * MathUtil.mdc(a,b);
+        final int obtido = MathUtil.mdc(m * a, m * b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("mdc(a.b,c) = mdc(a,c).mdc(b,c)")
+    public void testMdcP5(){
+        final int a = 7;
+        final int b = 3;
+        final int c = 5;
+        final int esperado = MathUtil.mdc(a * b, c);
+        final int obtido = MathUtil.mdc(b,c);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Se mdc(a,b) = 1, então mdc(a.b,c) = mdc(a,c).mdc(b,c)")
+    public void testMdcP5Resultado1(){
+        final int a = 7;
+        final int b = 3;
+        final int c = 5;
+        final int esperado = MathUtil.mdc(a * b, c);
+        final int obtido = MathUtil.mdc(b,c);
+        assertEquals(esperado, 1);
+    }
+
+    @Test
+    @Description("mdc(a,b) = mdc(b,a)")
+    public void testMdcP6(){
+        final int a = 8;
+        final int b = 2;
+        final int esperado = MathUtil.mdc(a,b);
+        final int obtido = MathUtil.mdc(b,a);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Qualquer ou todo valor negativo deve resultar no mesmo que se fosse positivo")
+    public void testMdcTudoNegativoP7(){
+        final int a = -12;
+        final int b = -6;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Qualquer ou todo valor negativo deve resultar no mesmo que se fosse positivo")
+    public void testMdcTudoPositivoP7(){
+        final int a = 12;
+        final int b = 6;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Qualquer ou todo valor negativo deve resultar no mesmo que se fosse positivo")
+    public void testMdcANegativoP7(){
+        final int a = -12;
+        final int b = 6;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    @Description("Qualquer ou todo valor negativo deve resultar no mesmo que se fosse positivo")
+    public void testMdcBNegativoP7(){
+        final int a = 12;
+        final int b = -6;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
 }
